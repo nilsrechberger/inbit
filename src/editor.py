8428368e59de1f86290b7b92fbf98b4a55407e24
Nils Rechberger <nilsrechberger@outlook.com>
2026-01-01 20:17:52 +0100

    feat: auto namespaces extraction

--- src/editor.py
+++ src/editor.py
@@ -1,8 +1,20 @@
+import defusedxml.ElementTree as ET
+
 class Editor():
     """ Editor class """
 
     def _init__(self) -> None:
-        pass
+        self.namespace = self._get_namespaces(self.input_file)
+        self.input_file = ET.parse(self.file)
+        self.output_file = None
+
+    def _get_namespaces(self, input_file) -> dict:
+        """ Extract namesapces from a XML file """
+        namespaces = {}
+        for _, elem in ET.iterparse(input_file, events=('start-ns')):
+            prefix, uri = elem
+            namespaces[prefix] = uri
+        return namespaces
 
     def edit_distance(tcx_file, distance) -> None:
         """ Edit distance in TCX file """
