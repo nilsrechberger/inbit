9cd618916467975770802c15cf2ba880154c1fd1
Nils Rechberger <nilsrechberger@outlook.com>
2026-01-01 19:33:20 +0100

    feat: initial commit

---
+++ src/editor.py
@@ -0,0 +1,9 @@
+class Editor():
+    """ Editor class """
+
+    def _init__(self) -> None:
+        pass
+
+    def edit_distance(tcx_file, distance) -> None:
+        """ Edit distance in TCX file """
+        pass
